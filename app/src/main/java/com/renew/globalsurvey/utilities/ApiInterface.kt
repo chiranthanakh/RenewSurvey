ea2d765221b4cda7bc798809e9c148a2b21dcc92
chiranth <chiru95sit@gmail.com>
2024-12-08 18:35:39 +0530

    CBS r/a question correction

--- app/src/main/java/com/renew/globalsurvey/utilities/ApiInterface.kt
+++ app/src/main/java/com/renew/globalsurvey/utilities/ApiInterface.kt
@@ -167,9 +167,9 @@ interface ApiInterface {
 
     companion object {
         var retrofitService: ApiInterface?=null
-        //   val BASE_URL="https://devrenewsms.proteam.co.in/api/v2/"
-         val BASE_URL="https://renewsms.proteam.co.in/api/v2/"
-       //  val BASE_URL = "https://cookstove.renew.com//api/v2/"
+         //  val BASE_URL="https://devrenewsms.proteam.co.in/api/v2/"
+        // val BASE_URL="https://renewsms.proteam.co.in/api/v2/"
+         val BASE_URL = "https://cookstove.renew.com//api/v2/"
 
 
 
